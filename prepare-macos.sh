8825b292a3e2c5c73c99ed06537e8c1222398cd5
Przemyslaw Jan Pietrzak <przemyslaw.jan.pietrzak@gmail.com>
2019-06-13 13:42:50 +0200

    add keepassx to macos

--- prepare-macos.sh
+++ prepare-macos.sh
@@ -1,5 +1,6 @@
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &&\
-brew install just &&\
-brew install bat &&\
-brew install wget &&\
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install just
+brew install bat 
+brew install wget 
 brew install git-flow
+brew cask install keepassx
