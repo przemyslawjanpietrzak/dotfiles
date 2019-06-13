9e5a68e0cdd65997bf9486ab4c4228a5c31ac8d5
Przemyslaw Beigert <przemyslawbeigert@espeos-MacBook-Pro.local>
2019-06-13 16:42:28 +0200

    update macos script

--- prepare-macos.sh
+++ prepare-macos.sh
@@ -1,6 +1,67 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install just
-brew install bat 
-brew install wget 
-brew install git-flow
+brew install bat
+brew install wget
+brew install midnight-commander
+brew install htop
+brew install googler
+brew install jq
+brew install tldr
+brew install fd 
+
+# browsers
+brew cask install brave-browser
+brew cask install caskroom/versions/firefox-developer-edition
+brew cask install google-chrome
+
+# dev tools
+brew cask install webstorm
+brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install slack
 brew cask install keepassx
+
+# window managers
+brew cask install amethyst
+
+# node
+brew install node
+Brew install yarn
+
+# git
+brew install git git-flow
+
+# python
+sudo easy_install pip
+pip install --user powerline-status
+pip install ipython
+brew install python3
+pip3 install ipython
+
+# zsh
+brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+echo 'zsh' >> .bash_profile
+
+# stuff
+mkdir code
+mkdir stuff
+git clone https://github.com/powerline/fonts
+cd fonts
+./install.sh
+cd ..
+Git clone https://github.com/wesbos/Cobalt2-iterm.git
+cd ..
+
+# ssh
+brew cask install tunnelblick
+brew install openssh
+
+# media
+brew cask install vlc
+brew install transmission
+brew cask install spotify
+
+# space-vim
+curl -sLf https://spacevim.org/install.sh | bash
