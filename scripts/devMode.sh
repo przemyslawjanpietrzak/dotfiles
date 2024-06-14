9c5a2b2dd1d8ecac4bd599d61a815b0f94a76adc
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2024-06-14 20:41:24 +0200

    add mode scripts

---
+++ scripts/devMode.sh
@@ -0,0 +1,9 @@
+#/bin/zsh
+
+
+open -a "Webstorm" &\
+open -a "KeePassXC" &\
+open -a "Slack" &\
+open -a "Brave Browser" &\
+open -a "Alacritty"
+
---
+++ scripts/normalMode.sh
@@ -0,0 +1,7 @@
+#/bin/zsh
+
+pkill "Webstorm" &\
+pkill "KeePassXC" &\
+pkill "Slack" &\
+pkill "Alacritty"
+
