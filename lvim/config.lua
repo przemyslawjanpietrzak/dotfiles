42c589063596a063341ee5b5ff946224a504761b
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2024-09-09 00:52:55 +0200

    update dotfiles

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,5 +1,23 @@
 lvim.plugins = {
-  "gbprod/nord.nvim"
+  "gbprod/nord.nvim",
+  "tpope/vim-fugitive",
+  "mg979/vim-visual-multi",
+  cmd = {
+    "G",
+    "Git",
+    "Gdiffsplit",
+    "Gread",
+    "Gwrite",
+    "Ggrep",
+    "GMove",
+    "GDelete",
+    "GBrowse",
+    "GRemove",
+    "GRename",
+    "Glgrep",
+    "Gedit"
+  },
+  ft = {"fugitive"}
 }
 
 vim.o.termguicolors = true
