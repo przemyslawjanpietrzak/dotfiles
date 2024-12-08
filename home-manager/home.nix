61e11c69dbcb7a02f21c5cd936130062d73baded
Przemyslaw Beigert <przemyslawbeigert@Przemyslaws-MacBook-Air.local>
2024-12-08 21:42:22 +0100

    fix git config

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,7 +14,7 @@
     # };
     ".config/aerospace/aerospace.toml".source = ./aerospace/aerospace.toml;
     ".config/alacritty/alacritty.toml".source = ./alacritty/alacritty.toml;
-    ".config/git/.gitconfig".source = ./git/.gitconfig;
+    ".config/git/config".source = ./git/config;
     ".config/lvim/config.lua".source = ./lvim/config.lua;
     ".config/ohmyposh/pk10-nord.toml".source = ./ohmyposh/p10k-nord.toml;
     ".config/tmux/tmux.conf".source = ./tmux/tmux.conf;
