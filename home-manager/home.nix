929d8520eb658ef8b8aa130dc2415e1578030520
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2024-12-11 16:14:20 +0100

    add global gitignore to home-manager

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -15,6 +15,7 @@
     ".config/aerospace/aerospace.toml".source = ./aerospace/aerospace.toml;
     ".config/alacritty/alacritty.toml".source = ./alacritty/alacritty.toml;
     ".config/git/config".source = ./git/config;
+    ".config/git/ignore".source = ./git/ignore;
     ".config/lvim/config.lua".source = ./lvim/config.lua;
     ".config/ohmyposh/pk10-nord.toml".source = ./ohmyposh/p10k-nord.toml;
     ".config/tmux/tmux.conf".source = ./tmux/tmux.conf;
