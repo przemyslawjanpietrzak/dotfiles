1990246388ac396487f8cc8ed21c6558f1f8e937
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2025-01-12 22:42:18 +0100

    clean up

--- hm/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    fish_vi_key_bindings
-    oh-my-posh init fish --config ~/.config/ohmyposh/pk10-nord.toml | source
-    eval "$(atuin init fish --disable-up-arrow)"
-    eval "$(zoxide init fish)"
-    set fish_greeting
-    function fish_mode_prompt; end
-end
