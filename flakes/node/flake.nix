898f7bece76dd1b8a483ab27c05dbed8bba73532
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2025-01-18 21:11:44 +0100

    add some example flakes

--- flakes/node/flake.nix
+++ flakes/node/flake.nix
@@ -9,14 +9,12 @@
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
         devShell = with pkgs;
         mkShell {
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          buildInputs = [ nodejs_22 ];
+          shellHook = "fish";
         };
       }
     );
