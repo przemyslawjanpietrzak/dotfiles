bce2a940ee78a108deaa4ed19219bcff06578895
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2025-03-14 09:20:09 +0100

    add ideavim config to home manager

--- home-manager/common/home.nix
+++ home-manager/common/home.nix
@@ -5,17 +5,18 @@
   home.file = {
     ".config/aerospace/aerospace.toml".source = ./aerospace/aerospace.toml;
     ".config/alacritty/alacritty.toml".source = ./alacritty/alacritty.toml;
+    ".config/fish/config.fish".source = ./fish/config.fish;
     ".config/git/config".source = ./git/config;
     ".config/git/ignore".source = ./git/ignore;
-    ".config/fish/config.fish".source = ./fish/config.fish;
+    ".config/i3/config".source = ./i3/config;
     ".config/lvim/config.lua".source = ./lvim/config.lua;
     ".config/ohmyposh/pk10-nord.toml".source = ./ohmyposh/p10k-nord.toml;
-    ".config/tmux/tmux.conf".source = ./tmux/tmux.conf;
-    ".config/zellij/config.kdl".source = ./zellij/config.kdl;
-    ".config/zed/settings.json".source = ./zed/settings.json;
-    ".config/i3/config".source = ./i3/config;
     ".config/rofi/config.rasi".source = ./rofi/config.rasi;
     ".config/rofi/nord.rasi".source = ./rofi/nord.rasi;
+    ".config/tmux/tmux.conf".source = ./tmux/tmux.conf;
+    ".config/zed/settings.json".source = ./zed/settings.json;
+    ".config/zellij/config.kdl".source = ./zellij/config.kdl;
+    ".ideavimrc".source = ./ideavim/.ideavimrc;
   };
 
   programs.home-manager.enable = true;
