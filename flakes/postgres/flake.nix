ec788ba37553b4d6c6b22b7367f7c9587670ad80
Your Name <set>
2025-06-02 16:41:22 +0200

    add postgres flake

---
+++ flakes/postgres/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "python-psycopg-dev-shell";
+          buildInputs = [
+            pkgs.python313
+            pkgs.postgresql
+            pkgs.libffi
+            pkgs.pkg-config
+          ];
+          shellHook = ''
+            echo "Welcome to the Python + PostgreSQL dev shell!"
+            echo "pg_config is available: $(which pg_config)"
+          '';
+        };
+      }
+    );
+}
